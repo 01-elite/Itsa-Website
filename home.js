a3db97ce23f55b4433163b81631d19f0f23380db
Sayali Gaikwad <sayaligaikwad838@gmail.com>
2025-08-17 13:44:29 +0530

    Itsa website 2025

---
+++ home.js
@@ -0,0 +1,28 @@
+const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        } else {
+          entry.target.classList.remove("show"); // revert when out of view
+        }
+      });
+    }, { threshold: 0.3 }); // trigger when 30% is visible
+
+    document.querySelectorAll(".animate-text").forEach(el => observer.observe(el));
+  
+    // cursor movement
+
+    let inCursor=document.querySelector(".innerCursor");
+    let outCursor=document.querySelector(".outerCursor");
+
+    document.addEventListener("mousemove", moveCursor);
+
+    function moveCursor(e) {
+      let x=e.clientX;
+       let y=e.clientY;
+       inCursor.style.left=`${x}px`;
+       inCursor.style.top=`${y}px`;
+
+       outCursor.style.left=`${x}px`;
+       outCursor.style.top=`${y}px`;
+    }
